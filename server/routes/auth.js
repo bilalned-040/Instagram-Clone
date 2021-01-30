8575d4a576c39da6122e1723d21e0caead065412
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2021-01-31 01:59:27 +0500

    Post route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,8 @@ const mongoose= require('mongoose');
 const User = mongoose.model("User");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {JWT_SECRET} = require('../keys')
+const {JWT_SECRET} = require('../keys');
+const requireLogin = require('../middleware/requireLogin')
 
 
 router.post('/signup',(req,res)=>{
