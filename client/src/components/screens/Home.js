598f23e83f3e775f4f0e2f7b1bbee2a7e3a1db53
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2021-02-05 17:32:17 +0500

    Comments and delete post

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -67,13 +67,62 @@ const Home  = ()=>{
             console.log(err)
         })
     }
+
+    const makeComment = (text,postId)=>{
+        fetch('/comment',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.map(item=>{
+              if(item._id==result._id){
+                  return result
+              }else{
+                  return item
+              }
+           })
+          setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const deletePost = (postid)=>{
+        fetch(`/deletepost/${postid}`,{
+            method:"delete",
+            headers:{
+                Authorization:"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.filter(item=>{
+                return item._id !== result._id
+            })
+            setData(newData)
+        })
+    }
     return (
         <div className="home">
             {
                 data.map(item=>{
                     return(
                         <div className="card home-card" key={item._id}>
-                             <h5>{item.postedBy.name}</h5>
+                             <h5>{item.postedBy.name}{item.postedBy._id == state._id 
+                            && <i className="material-icons" style={{
+                                float:"right"
+                            }} 
+                            onClick={()=>deletePost(item._id)}
+                            >delete</i>
+
+                            }</h5>
                              <div className="card-image">
                                  <img src={item.photo}/>
                              </div>
@@ -92,8 +141,19 @@ const Home  = ()=>{
                                 <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
-
-                                <input type="text" placeholder="add a comment" /> 
+                                {
+                                    item.comments.map(record=>{
+                                        return(
+                                        <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+                                        )
+                                    })
+                                }
+                                <form onSubmit={(e)=>{
+                                    e.preventDefault()
+                                    makeComment(e.target[0].value,item._id)
+                                }}>
+                                  <input type="text" placeholder="add a comment" />  
+                                </form>
                             </div> 
                         </div>
                     )
